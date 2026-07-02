7e64f7215b1a27917c0c6a5caecddefae65a650c
toolsrift <toolsrift@gmail.com>
2026-07-02 18:39:28 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -16,7 +16,6 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
       </Head>
 
       {/* Google Analytics */}
